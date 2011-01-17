ef3226f29e6f055948b219cbc6644414a8a34508
charlotte.galzy@gmail.com <charlotte.galzy@gmail.com@3b071300-6fcd-2378-73ba-bf40880a43e5>
2011-01-17 16:37:01 +0000

    Corrections des différentes fonctions de vérification et du main.

--- trunk/ETL/Verification_excel/src/verification_excel/Main.java
+++ trunk/ETL/Verification_excel/src/verification_excel/Main.java
@@ -1,32 +1,43 @@
 package verification_excel;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.poi.hssf.extractor.ExcelExtractor;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-
 public class Main {
 
     public static void main(String[] args) {
         String dossier = "./";
-        Verification verif = new Verification(dossier);
-        //Ouverture du fichier :
-		System.out.println("Ouverture fichier : " + verif.Ouverture_fichier());
-		// Vérification de l'existence des 4 onglets :
-        System.out.println("Verif onglets : " + verif.verification_onglets());
-		// Vérification de l'onglet référentiel :
-        System.out.println("Verif referentiel : " + verif.Verification_referentiel());
-		// Vérification des onglets concernant le matériel :
-		System.out.println("Verif materiel : " + verif.Verification_materiels());
+		String[] resultat = {	"0 - Pas de probleme",
+								"1 - Probleme a l ouverture du fichier (extension, chemin,...)",
+								"2 - Le fichier est corrompu", "3 - Probleme dans le nom des onglets",
+								"4 - Erreur sur le nombre de colonnes de l onglet 1",
+								"5 - Erreur sur le titre d une colonne de l onglet 1",
+								"6 - Probleme sur la donnee Action de l onglet 1",
+								"7 - Probleme d acces au contenu d une cellule de l onglet 1",
+								"8 - Erreur sur le nombre de colonnes d un des onglets concernant le materiel",
+								"9 - Erreur sur le titre d une colonne d un des onglets concernant le materiel",
+								"10 - L annee n est pas correcte sur un des onglets concernant le materiel",
+								"11 - Probleme dans le nombre de mois d un des onglets concernant le materiel",
+								"12 - Probleme d acces a une cellule d un des onglets concernant le materiel"};
+        try {
+			Verification verif = new Verification(dossier);
+			//Ouverture du fichier :
+			int ouverture_fichier = verif.Ouverture_fichier();
+			System.out.println("Ouverture fichier : " + resultat[ouverture_fichier]);
+			if(ouverture_fichier == 0) {
+				// Vérification de l'existence des 4 onglets :
+				int verification_onglets = verif.Verification_onglets();
+				System.out.println("Verif onglets : " + resultat[verification_onglets]);
+				if(verification_onglets == 0) {
+					// Vérification de l'onglet référentiel :
+					int verification_referentiel = verif.Verification_referentiel();
+					System.out.println("Verif referentiel : " + resultat[verification_referentiel]);
+					if (verification_referentiel == 0) {
+						// Vérification des onglets concernant le matériel :
+						System.out.println("Verif materiel : " + resultat[verif.Verification_materiels()]);
+					}
+				}
+			}
+		} catch(Exception e) {
+
+		}
     }
 
 }
--- trunk/ETL/Verification_excel/src/verification_excel/Verification.java
+++ trunk/ETL/Verification_excel/src/verification_excel/Verification.java
@@ -8,7 +8,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
@@ -17,10 +16,11 @@ public class Verification {
     private int annee_actuelle;
 	private HSSFWorkbook classeur_excel;
     private String chemin;
-    private String[] referentiel_titres = {"Ville","Action","Pays","Continent","Devise","Enseigne",
-											"Publicite","Region","Emplacement","Adresse","Horaires_Ouverture",
-											"Nb_Caisses","Population","Taux_Ouvrier","Taux_Cadre","Taux_Inactif",
-											"Moins_25ans","Les_25_35ans","Plus_35ans"};
+	private String[] onglets = {"Referentiel", "Fours", "Magnetoscopes", "Hifi"};
+    private String[] referentiel_titres = {"Ville", "Action", "Pays", "Continent", "Devise", "Enseigne",
+											"Publicite", "Region", "Emplacement", "Adresse", "Horaires_Ouverture",
+											"Nb_Caisses", "Population", "Taux_Ouvrier", "Taux_Cadre", "Taux_Inactif",
+											"Moins_25ans", "Les_25_35ans", "Plus_35ans"};
 	private String[] materiel_titres = {"Ville", "Annee", "Mois", "O_Ventes", "O_CA", "O_MB"};
 	
 	Verification(String d) {
@@ -45,118 +45,103 @@ public class Verification {
 				classeur_excel = new HSSFWorkbook(new POIFSFileSystem(fic));
 			}
 			catch (IOException ex) {
-			   return 2; // Le fichier est corrumpu,...
+				return 2; // Le fichier est corrumpu, ...
 			}
 		}
 		catch (FileNotFoundException ex) {
-		 return 1; // Erreur sur l'ouverture du fichier: problème d'extension,chemin,...
+			return 1; // Erreur sur l'ouverture du fichier : problème d'extension, chemin, ...
 		}
-      return 0;
+		return 0;
     }
 
-    public int verification_onglets()
-    {
-		Sheet feuille1 = classeur_excel.getSheetAt(0); // Récupération du nom des feuilles
-		if (feuille1 != null) {
-			String nom_feuille1 = feuille1.getSheetName();
-			if(nom_feuille1.compareTo("Referentiel") != 0) {
-				return 3; // Le nom de la feuille 1 est incorrect
-			}
-		}
-		Sheet feuille2 = classeur_excel.getSheetAt(1); // Récupération du nom des feuilles
-		if (feuille2 != null) {
-			String nom_feuille2 = feuille2.getSheetName();
-			if(nom_feuille2.compareTo("Fours") != 0) {
-				return 4; // Le nom de la feuille 2 est incorrect
-			}
-		}
-		Sheet feuille3 = classeur_excel.getSheetAt(2); // Récupération du nom des feuilles
-		if (feuille3 != null) {
-			String nom_feuille3 = feuille3.getSheetName();
-			if(nom_feuille3.compareTo("Magnetoscopes")!=0) {
-				return 5; // Le nom de la feuille 3 est incorrect
-			}
-		 }
-		Sheet feuille4 = classeur_excel.getSheetAt(3); // Récupération du nom des feuilles
-		if (feuille4 != null) {
-			String nom_feuille4 = feuille4.getSheetName();
-			if(nom_feuille4.compareTo("Hifi")!= 0) {
-				return 6; // Le nom de la feuille 4 est incorrect
+    public int Verification_onglets() {
+		for(int i = 0; i < onglets.length; i++) {
+			Sheet feuille_actuelle = classeur_excel.getSheetAt(i); // Récupération de la feuille
+			if (feuille_actuelle != null) {
+				String nom_feuille_actuelle = feuille_actuelle.getSheetName(); // Récupération du nom de la feuille
+				if(nom_feuille_actuelle.compareTo(onglets[i]) != 0) {
+					return 3; // Le nom d'un onglet est incorrect
+				}
 			}
 		}
 		return 0;
      }
 	
      public int Verification_referentiel() {
-        int i = 0;
-        int retour = 0;
         Sheet feuille1 = classeur_excel.getSheetAt(0);
-        Row ligne1 = feuille1.getRow(0); // On se place sur la ligne1
-        for (Cell cell : ligne1) { // On parcourt cellule par cellule
-			if(referentiel_titres[i].compareTo(cell.getStringCellValue()) !=0 ) {
-               retour = 7; // Erreur sur le titre d'une colonne
+		Row ligne1 = feuille1.getRow(0); // On se place sur la ligne1
+		for(int colonne = 0; colonne < referentiel_titres.length; colonne++) {
+			try {
+				if(referentiel_titres[colonne].compareTo(ligne1.getCell(colonne).getStringCellValue()) != 0){
+					return 5; // Erreur sur le titre d'une colonne
+				}
+			} catch (Exception e) {
+				return 4; // Erreur sur le nombre de colonnes
 			}
-			i++;
+		}
+		
+        for(int i = 1; i < feuille1.getLastRowNum(); i++) {
+			try {
+				Row row = feuille1.getRow(i);
+				String action = row.getCell(1).getStringCellValue();
+				if(action.compareTo("A") != 0 && action.compareTo("M") != 0 && action.compareTo("S") != 0) {
+					return 6; // Problème sur la donnée Action
+				}
+			} catch (Exception e) {
+				return 7; // Probleme d'acces au contenu d'une cellule
+			}
         }
-
-        int j = 1;
-        for(j = 1; j < feuille1.getLastRowNum(); j++) {
-			Row row = feuille1.getRow(j);
-            String action = row.getCell(1).getStringCellValue();
-            if(action.compareTo("A") != 0 && action.compareTo("M") != 0 && action.compareTo("S") != 0) {
-                retour = 8; // Problème sur la donnée Action
-            }
-            
-        }
-        return retour;
+        return 0;
      }
 
      public int Verification_materiels() {
 		 int colonne = 0;
-		 int retour = 0;
 
 		 // Parcours des trois feuilles concernant les objectifs
-		 for (int index_feuille = 1; index_feuille < 4; index_feuille++) {
-			 Sheet feuille_actuelle = classeur_excel.getSheetAt(index_feuille); // Placement sur la feuille souhaitée
+		 for (int index_onglet = 1; index_onglet < 4; index_onglet++) {
+			 Sheet onglet_actuel = classeur_excel.getSheetAt(index_onglet); // Placement sur la feuille souhaitée
 
-			 String ville_actuelle = feuille_actuelle.getRow(1).getCell(0).getStringCellValue();
+			 String ville_actuelle = onglet_actuel.getRow(1).getCell(0).getStringCellValue();
 			 String ville_precedente = ville_actuelle;
 
 			 int mois = 1;
 
-			 // Vérification que le nombre de lignes correspond bien à ce qu'il y a dans la BDD
-
+			 // A FAIRE : Vérification que le nombre de lignes correspond bien à ce qu'il y a dans la BDD
 			 
-			 for(Row ligne : feuille_actuelle) {
+			 for(Row ligne : onglet_actuel) {
 				 if(ligne.getRowNum() ==  0) { // Si on est sur la première ligne, on vérifie les titres
 					 for(colonne = 0; colonne < materiel_titres.length; colonne++) {
 						 try {
 							 if(materiel_titres[colonne].compareTo(ligne.getCell(colonne).getStringCellValue()) != 0){
-								return 7; // Erreur sur le titre d'une colonne
+								return 9; // Erreur sur le titre d'une colonne
 							 }
 						 } catch (Exception e) {
 							 return 8; // Erreur sur le nombre de colonnes
 						 }
 					 }
 				 } else { // On n'est pas sur la ligne de titre, on vérifie les données
-					 if(ligne.getCell(1).getNumericCellValue() != annee_actuelle) {
-						 return 9; // L'année n'est pas correcte
-					 }
+					 try {
+						 if(ligne.getCell(1).getNumericCellValue() != annee_actuelle) {
+							 return 10; // L'année n'est pas correcte
+						 }
 
-					 ville_actuelle = ligne.getCell(0).getStringCellValue(); // Récupération de la ville courante
-					 if (ville_actuelle.compareTo(ville_precedente) != 0) { // Nouvelle ville
-						 mois = 1; // Le compteur de mois repart à 1
+						 ville_actuelle = ligne.getCell(0).getStringCellValue(); // Récupération de la ville courante
+						 if (ville_actuelle.compareTo(ville_precedente) != 0) { // Nouvelle ville
+							 mois = 1; // Le compteur de mois repart à 1
+						 }
+						 if (ligne.getCell(2).getNumericCellValue() != mois) { // Le mois n'est pas celui attendu
+							 return 11; // Problème dans le nombre de mois
+						 }
+
+						 mois++;
+						 ville_precedente = ligne.getCell(0).getStringCellValue();
+					 } catch (Exception e) {
+						 return 12; // Problème d'accès à une cellule
 					 }
-					 if (ligne.getCell(2).getNumericCellValue() != mois) { // Le mois n'est pas celui attendu
-						 return 10; // Problème dans le nombre de mois
-					 }
-					 
-					 mois++;
-					 ville_precedente = ligne.getCell(0).getStringCellValue();
 				  }
 				  colonne = 0; // Remise à 0 de l'index pointant sur la colonne (pour les prochains onglets)
 			 }
 		 }
-		 return retour;
+		 return 0;
      }
 }
