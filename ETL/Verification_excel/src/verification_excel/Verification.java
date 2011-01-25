b18b9705ea61c0b38f1d66122f3fa1ff17c419b3
charlotte.galzy <charlotte.galzy@gmail.com>
2011-01-25 15:13:45 +0000

    Mise à jour et fin du programme de vérification des fichiers Excel pour les alimentations annuelle et mensuelle.

--- ETL/Verification_excel/src/verification_excel/Verification.java
+++ ETL/Verification_excel/src/verification_excel/Verification.java
@@ -3,7 +3,8 @@ package verification_excel;
 public abstract class Verification {
 
 	public abstract int Ouverture_fichier();
+	public abstract String getResultat(int index);
 	public abstract int Verification_onglets();
 	public abstract int Verification_referentiel();
-	public abstract int Verification_materiels();
+	public abstract int Verification_objectifs();
 }
